f522411cf935557cf4d3fdcbb8483554558b9c88
rashel tankilevich <rashel2468@gmail.com>
2021-09-28 16:20:58 +0300

    search apartments - js + ajax

---
+++ AirDnT/wwwroot/js/search.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.btnSearch').click(function () {
+        var displayName = $('#displayName');
+        $.ajax({
+            url: "/Apartments/Search",
+            data: { DisplayName: displayName.val() }
+        }).done(function (result) {
+           
+        });
+    });
+});
