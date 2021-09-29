707beee0facccb3a0f750f81e5a4c6629c86d931
rashel tankilevich <rashel2468@gmail.com>
2021-09-29 16:20:53 +0300

    apartments search result as json

--- AirDnT/wwwroot/js/search.js
+++ AirDnT/wwwroot/js/search.js
@@ -5,7 +5,16 @@
             url: "/Apartments/Search",
             data: { DisplayName: displayName.val() }
         }).done(function (result) {
-           
+            var tbody = $('tbody');
+            var template = $('#template').html();
+            tbody.html('');
+            $.each(result, function (key, value) {
+                var temp = template;
+                $.each(value, function (key, value) {
+                    temp = temp.replaceAll('${' + key + '}', value);
+                });
+                tbody.append(temp);
+            });
         });
     });
 });
