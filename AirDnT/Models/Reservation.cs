1cd7e971cf164672fd9edc89f731689de848815c
dani akuli <daniakuli2@gmail.com>
2021-10-30 12:55:32 +0300

    Added Reservations + date validation

---
+++ AirDnT/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirDnT.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int ApartmentID { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Start date")]
+        public DateTime sAvailability { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "End date")]
+        public DateTime eAvailability { get; set; }
+
+        public virtual Customer Customers { get; set; }
+
+        public virtual Apartment Apartment { get; set; }
+    }
+}
---
+++ AirDnT/wwwroot/js/checkDate.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    var today = new Date();
+
+    var day = ("0" + today.getDate()).slice(-2);
+    var month = ("0" + (today.getMonth() + 1)).slice(-2);
+
+    var thisDate = today.getFullYear() + "-" + (month) + "-" + (day);
+
+    $(".changedDate").attr('min', thisDate).change(function () {
+        var sDate = document.getElementById("startDate").value;
+        var eDate = document.getElementById("endDate").value;
+        if (eDate != "" || sDate != "") {
+            if (Date.parse(sDate) >= Date.parse(eDate) || sDate == "") {
+                document.getElementById("startDate").value = eDate;
+                document.getElementById("endDate").value = "";
+            }
+        }
+        else {
+            var sDate = document.getElementById("sDate").value;
+            var eDate = document.getElementById("eDate").value;
+            if (eDate != "" || sDate != "") {
+                if (Date.parse(sDate) >= Date.parse(eDate) || sDate == "") {
+                    document.getElementById("sDate").value = eDate;
+                    document.getElementById("eDate").value = "";
+                }
+            }
+        }
+    });
+})
